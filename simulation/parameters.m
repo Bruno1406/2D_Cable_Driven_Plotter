99755ff14f516866d717916c770eb31587506fee
Bruno1406 <bruno.lossmachado@gmail.com>
2025-05-30 11:10:31 -0300

    Add Simulation

---
+++ simulation/parameters.m
@@ -0,0 +1,15 @@
+% Define plotter parameters as Simulink.Parameter objects
+
+K_t = 0.009;
+L_a = 0.0023;
+R_a = 2.182;
+J_m = 0.000004;
+B_m = 0.000002;
+Gr  = 1/30;
+L   = 0.3;
+K   = 1050;
+C   = 3;
+m   = 0.1;
+g   = 9.8;
+R   = 0.017;
+alfa = cos(0.34906585);
