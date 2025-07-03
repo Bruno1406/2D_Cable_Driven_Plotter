be4d8a2138dad71c79ca86a559d989c8e7331e5c
Bruno1406 <bruno.lossmachado@gmail.com>
2025-07-03 19:40:20 -0300

    a

--- simulation/parameters.m
+++ simulation/parameters.m
@@ -1,10 +1,10 @@
 % Define plotter parameters as Simulink.Parameter objects
 
-K_t = 0.0022818279;
+K_t = 0.01;
 L_a = 0.0048436939;
-R_a = 2.182;
+R_a = 2.2;
 J_m = 0.000004;
-B_m = 0.0129625645;
+B_m = 0.000001;
 Gr  = 1/30;
 L   = 0.3;
 K   = 2040;
@@ -16,11 +16,13 @@ alfa = cos(0.34906585);
 tick_p_meter = 17975.15;
 compare_to_volt = 0.01171875;
 
-set_points = readtable('setpoint.csv');
+set_points = readtable('set_points.csv');
 % Extract the columns into variables
-t = set_points.t;
-left_ticks = set_points.tick_left;
-right_ticks = set_points.tick_right;
+t = set_points.time;
+left_ticks = set_points.left_ticks;
+right_ticks = set_points.right_ticks;
 
 left_signal = [t,left_ticks];
 right_signal = [t, right_ticks];
+
+plot(t,left_ticks)
